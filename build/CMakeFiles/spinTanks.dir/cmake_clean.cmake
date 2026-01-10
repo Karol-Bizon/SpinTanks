e228721aa877763998cdb8a5770e97fae628b076
w-maks <wmaksymiak@student.agh.edu.pl>
2026-01-10 19:53:45 +0100

    gitignore

---
+++ build/CMakeFiles/spinTanks.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spinTanks.dir/link.d"
+  "CMakeFiles/spinTanks.dir/src/game.cpp.o"
+  "CMakeFiles/spinTanks.dir/src/game.cpp.o.d"
+  "CMakeFiles/spinTanks.dir/src/main.cpp.o"
+  "CMakeFiles/spinTanks.dir/src/main.cpp.o.d"
+  "CMakeFiles/spinTanks.dir/src/tank.cpp.o"
+  "CMakeFiles/spinTanks.dir/src/tank.cpp.o.d"
+  "spinTanks"
+  "spinTanks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spinTanks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/spinTanks.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for spinTanks.
+# This may be replaced when dependencies are built.
